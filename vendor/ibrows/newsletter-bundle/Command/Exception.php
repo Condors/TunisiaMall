2dcfe680671b5f2e2a629dba3664eaa8093c7ad4
sooheib <souheib.selmi@esprit.tn>
2016-05-14 11:09:08 +0100

    update files

--- vendor/ibrows/newsletter-bundle/Command/Exception.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace Ibrows\Bundle\NewsletterBundle\Command;
-
-class Exception extends \Exception
-{ }
--- vendor/ibrows/newsletter-bundle/Renderer/RendererInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Ibrows\Bundle\NewsletterBundle\Renderer;
-
-interface RendererInterface
-{
-    public function render(RenderableInterface $element, array $parameters = array());
-}
